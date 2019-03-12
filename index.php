8f853e73f2d993a0048bcee49d243eb8a2489e4b
fachrezi <40844080+fachrezi@users.noreply.github.com>
2019-03-12 15:58:03 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-    echo"<h1>Hello World</h1> <br/>";
-    
-    $text = "<table>
-    <tr>
-        <td>NIM</td>
-        <td> : </td>
-        <td>16.01.53.0064</td>
-    </tr>
-    <tr>
-        <td>Nama</td>
-        <td> : </td>
-        <td>Dimas Ahmad Rizal</td>
-    </tr>
-</table> <br>";
+echo "Hello World";
+echo "<br>";
+echo "Nama	: Nabil Fachrezi";
+echo "<br>";
+echo "NIM	: 16.01.53.0135";
+echo "<br>";
+echo "Progdi: Teknik Informatika";
+?> 
 
-    echo $text;
-?>
+<a href="/akademik">Go To Akademik</a>
 
-    <a href="/akademik">Go To Akademik</a>
+</body>
+</html>
